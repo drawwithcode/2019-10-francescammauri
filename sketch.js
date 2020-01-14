7d676a0aae464a959f44fdb6036d93e89597766f
francescammauri <>
2020-01-14 18:07:17 +0100

    Uploaded last assignment

--- sketch.js
+++ sketch.js
@@ -1,11 +1,70 @@
-function preload(){
-  // put preload code here
+let angle=0;
+ {
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+
+
 }
 
 function draw() {
-  // put drawing code here
+
+
+let locX = mouseX ;
+let locY = mouseY ;
+
+background("black");
+orbitControl();
+camera(locX, locY, 10, 50, 0, 0, 10, 50, 0)
+
+push()
+stroke("white");
+rotateX(angle * 0.1)
+rotateZ(angle * 0.1)
+lightFalloff(1, 0, 0);
+pointLight("aquamarine", locX, locY, 50);
+box(500,500, 10)
+pop()
+push()
+rotateX(angle * 0.3)
+rotateZ(angle * 0.2)
+lightFalloff(1,0,0);
+pointLight("red", locX, locY, 50);
+translate(-150, 150)
+box(10,500, 500)
+pop()
+
+push()
+normalMaterial();
+noStroke();
+rotateX(angle);
+torus(50, 10);
+specularColor("red");
+rotateZ(angle * 0.0001)
+box(30);
+
+pop()
+
+angle+= 0.5
+
+
+
+
+
+
+
+
+
+//pointLight(0,0, 255, mouseX - 200, mouseY-200, 0)
+//pointLight(0,0, 255, mouseX - 200, mouseY-200, 0)
+//background(170);
+//rectMode(CENTER);
+//rotateX(angle);
+//translate(mouseX- width/2, mouseY - height/2);
+//ambientMaterial(255);
+//noStroke();
+//cylinder(10, 300);
+
 }
